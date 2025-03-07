b79c3e7354a4cd96ddc004113715ee39a12c89a7
Master <135227616+ArtMastar@users.noreply.github.com>
2025-03-07 12:53:40 +0000

    add even or odd number checker in evenorodd.py

---
+++ the-easy-way/evenorodd.py
@@ -0,0 +1,6 @@
+num  = int(input("Let's see if your numnber is even or odd. Enter a number: "))
+
+if num % 2 == 0:
+    print("Your number " ,num, " is even.")
+else:
+    print("Your number " ,num, " is odd.")
